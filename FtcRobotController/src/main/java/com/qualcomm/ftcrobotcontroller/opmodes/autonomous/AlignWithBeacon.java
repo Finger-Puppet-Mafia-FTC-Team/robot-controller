0af298f49b1a71d6aaaa89843e3ffc7402b6a111
zodern <zodern@users.noreply.github.com>
2016-02-24 09:43:17 -0600

    Fix align with beacon
    It uses only one wheel to keep in front of the beacon. It also didn’t
    have enough power.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/AlignWithBeacon.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/AlignWithBeacon.java
@@ -18,16 +18,19 @@ public class AlignWithBeacon extends step{
     @Override
     public void runStep (Autonomous2 instance, AutonomousHardware hardware) {
         double difference = hardware.sonicLeft.getUltrasonicLevel() - hardware.sonicRight.getUltrasonicLevel();
-        if(Math.abs(difference) < 1) {
+        if(Math.abs(difference) == 0) {
             done = true;
             return;
         }
+
+        // we only move one motor since we want to stay on the tape and the sensor is not in the center
         if(difference > 0) {
-            hardware.motorRight.setPower(0.38);
-            hardware.motorLeft.setPower(-0.38);
+            //hardware.motorRight.setPower(0.38);
+            hardware.motorLeft.setPower(-0.4);
         } else if (difference < 0) {
-            hardware.motorRight.setPower(-0.38);
-            hardware.motorLeft.setPower(0.38);
+           // hardware.motorRight.setPower(-0.38);
+            hardware.motorLeft.setPower(0.4);
         }
+        hardware.motorRight.setPower(0);
     }
 }
