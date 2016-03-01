a56d588a9cc0be986d8067c4c005a7d44a5a3c50
zodern <zodern@users.noreply.github.com>
2016-03-01 15:03:01 -0600

    drive close is more reliable

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/DriveClose.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/DriveClose.java
@@ -17,11 +17,15 @@ public class DriveClose extends step{
 
     @Override
     public void runStep (Autonomous2 instance, AutonomousHardware hardware) {
-        double distance = hardware.sonicLeft.getUltrasonicLevel();
-        if(distance > 12) {
-            hardware.motorRight.setPower(-0.4);
-            hardware.motorLeft.setPower(-0.4);
+        double distanceLeft = hardware.sonicLeft.getUltrasonicLevel();
+        double distanceRight = hardware.sonicRight.getUltrasonicLevel();
+        double distance = Math.min(distanceLeft, distanceRight);
+        if(distance > 13) {
+            hardware.motorRight.setPower(-0.37);
+            hardware.motorLeft.setPower(-0.37);
         } else {
+            hardware.motorLeft.setPower(0);
+            hardware.motorRight.setPower(0);
             done = true;
         }
     }
