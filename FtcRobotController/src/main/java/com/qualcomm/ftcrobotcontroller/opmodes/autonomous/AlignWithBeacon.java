4314782e1b0dc00138cdea9725df4a09bfadacf8
zodern <zodern@users.noreply.github.com>
2016-03-03 15:35:42 -0600

    logging ultrasonic sensor in alignwithbeacon

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/AlignWithBeacon.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/AlignWithBeacon.java
@@ -20,6 +20,11 @@ public class AlignWithBeacon extends step{
     @Override
     public void runStep (Autonomous2 instance, AutonomousHardware hardware) {
         double difference = hardware.sonicLeft.getUltrasonicLevel() - hardware.sonicRight.getUltrasonicLevel();
+
+        Log.i("sonic", "sonicright" + hardware.sonicRight.getUltrasonicLevel());
+        Log.i("sonic", "soniceleft" + hardware.sonicLeft.getUltrasonicLevel());
+        Log.i("difference", String.valueOf(difference));
+
         if(Math.abs(difference) == 0) {
             Log.i("test", "soniceleft" + hardware.sonicLeft.getUltrasonicLevel());
             Log.i("test", "sonicright" + hardware.sonicRight.getUltrasonicLevel());
