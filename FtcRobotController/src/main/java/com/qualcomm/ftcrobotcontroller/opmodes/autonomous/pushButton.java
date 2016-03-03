bf9d5dc5749f8daf84ffa456ea411d5d87160d58
zodern <zodern@users.noreply.github.com>
2016-03-03 15:36:23 -0600

    Only push button if red. Stop after 3 seconds

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/pushButton.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous/pushButton.java
@@ -2,9 +2,12 @@ package com.qualcomm.ftcrobotcontroller.opmodes.autonomous;
 
 import com.qualcomm.ftcrobotcontroller.opmodes.Autonomous2;
 import com.qualcomm.ftcrobotcontroller.opmodes.AutonomousHardware;
+import java.util.Date;
 
-public class pushButton extends step{
+
+public class pushButton extends step {
     String target;
+    long stepStartTime = 0;
     boolean didInit = false;
 
     @Override
@@ -13,20 +16,22 @@ public class pushButton extends step{
     }
 
     @Override
-    public  void initStep (Autonomous2 instance, AutonomousHardware hardware) {
-        if(didInit) {
+    public void initStep(Autonomous2 instance, AutonomousHardware hardware) {
+        if (didInit) {
             return;
         }
         didInit = true;
+
+        stepStartTime = new Date().getTime();
 
         int red = hardware.topColor.red();
         int blue = hardware.topColor.blue();
 
         boolean isBlue = blue > red;
 
-        if(instance.getIsBlue() && isBlue) {
+        if (instance.getIsBlue() && isBlue) {
             target = "right";
-        } else if(!instance.getIsBlue() && !isBlue) {
+        } else if (!instance.getIsBlue() && !isBlue) {
             target = "right";
         } else {
             target = "left";
@@ -35,15 +40,28 @@ public class pushButton extends step{
 
 
     @Override
-    public void runStep (Autonomous2 instance, AutonomousHardware hardware) {
+    public void runStep(Autonomous2 instance, AutonomousHardware hardware) {
         instance.addMessage(target);
-        if(target == "right") {
+
+
+        if (instance.getIsBlue()) {
+            done = true;
+            return;
+        }
+
+        if(new Date().getTime() - stepStartTime > 3000) {
+            done = true;
+            return;
+        }
+
+        if (target == "right") {
             hardware.motorRight.setPower(-0.4);
             hardware.motorLeft.setPower(0);
         } else {
             hardware.motorLeft.setPower(-0.4);
             hardware.motorRight.setPower(0.4);
         }
+        hardware.collector.setPower(0);
     }
 }
 
