a9b2dd01f4915ca116778c8f7916b51e42b6afb7
zodern <zodern@users.noreply.github.com>
2016-03-11 11:16:38 -0600

    Change registered name of teleop

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -60,7 +60,7 @@ public class FtcOpModeRegister implements OpModeRegister {
 
         // manager.register("NullOp", NullOp.class);
         // manager.register("motorcontrollertest", motorcontrollertest.class);
-        manager.register("TeleOp Competition Dec. 12", teleop.class);
+        manager.register("TeleOp Super Regionals", teleop.class);
        // manager.register("Camera", CameraOp.class);
        // manager.register("Fun", fun.class);
         manager.register("Autonomous Blue", AutonomouseDebug.class);
